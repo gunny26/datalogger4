bb98aa87777f80168396983c682f513e78e3e0aa
Arthur Messner <arthur.messner@tirol-kliniken.at>
2016-04-01 10:44:56 +0200

    calling tsastats, to generate statistics also in first run

--- bin/generate_tsas.py
+++ bin/generate_tsas.py
@@ -20,7 +20,7 @@ def main(project, tablename, datestring):
     else:
         if len(caches["tsa"]["keys"]) == 0:
             logging.info("%s TSA Archive missing, calling get_tsa and get_tsastats", suffix)
-            data = datalogger.load_tsa(datestring)
+            data = datalogger.load_tsastats(datestring)
         else:
             if len(caches["tsastat"]["keys"]) == 0:
                 logging.info("%s TSASTAT Archive missing, calling get_tsastats", suffix)
@@ -28,7 +28,7 @@ def main(project, tablename, datestring):
             else:
                 if len(caches["ts"]["keys"]) == 0:
                     logging.info("%s there are no ts archives, something went wrong, or tsa is completely empty, calling get_tsastats", suffix)
-                    data = datalogger.load_tsa(datestring)
+                    data = datalogger.load_tsastats(datestring)
                 else:
                     logging.info("%s All fine", suffix)
     del data
